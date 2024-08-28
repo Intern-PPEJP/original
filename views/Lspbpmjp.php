b35b50656566adcc81b39676725d610a3b474747
ansalma <anandaslm04@gmail.com>
2024-08-28 10:10:06 +0700

    lspbpmjp benerin media query

--- views/Lspbpmjp.php
+++ views/Lspbpmjp.php
@@ -75,7 +75,7 @@ p, table, div {
     .custom-card {
     display: inline-block;
     padding: 25px 20px 10px 20px; /* Padding atas 25px, bawah 10px, kiri/kanan 20px */
-    background-color: #fcf6bd;  Warna latar card */
+    background-color: #fcf6bd;  /*Warna latar card */
 }
 
 
@@ -101,20 +101,39 @@ p, table, div {
 <div class="container" style="padding-bottom: 0px;">
     <div class="row my-4">
         <div class="col-md-12">
-            <!--<h3 class="mb-3 text-bold text-center">LSP P1 BPMJP</h3>-->
             <div class="text-justify">
-				<p style="margin-left: 125px; margin-right: 125px; padding-bottom: 0px;"> Lembaga Sertifikasi Profesi (LSP) adalah organisasi yang berperan dalam sertifikasi kompetensi kerja di Indonesia. LSP berfungsi untuk menguji dan menilai kompetensi seseorang berdasarkan standar kompetensi yang telah ditetapkan oleh Badan Nasional Sertifikasi Profesi (BNSP) atau lembaga terkait lainnya. LSP bertujuan untuk memastikan bahwa tenaga kerja memiliki kompetensi sesuai dengan kebutuhan industri dan pasar kerja. </p>
+				<p class="responsive-text" style="padding-bottom: 0px;"> 
+                Lembaga Sertifikasi Profesi (LSP) adalah organisasi yang berperan dalam sertifikasi kompetensi kerja di Indonesia. LSP berfungsi untuk menguji dan menilai kompetensi seseorang berdasarkan standar kompetensi yang telah ditetapkan oleh Badan Nasional Sertifikasi Profesi (BNSP) atau lembaga terkait lainnya. LSP bertujuan untuk memastikan bahwa tenaga kerja memiliki kompetensi sesuai dengan kebutuhan industri dan pasar kerja. 
+                </p>
 			</div>
         </div>
     </div>
 </div>
+
+<style>
+/* Untuk layar dengan lebar lebih besar dari 768px */
+.responsive-text {
+    margin-left: 125px;
+    margin-right: 125px;
+    padding-bottom: 0px;
+}
+
+/* Untuk layar dengan lebar lebih kecil dari 768px */
+@media (max-width: 768px) {
+    .responsive-text {
+        margin-left: 30px;
+        margin-right: 30px;
+    }
+}
+</style>
+
 <div class="d-flex justify-content-center">
-<div class="col-md-5">
+<div class="col-md-8 col-12">
     <div style="color:#000;padding-top:10px;padding-bottom:3px;">
         <h2 class="text-bold text-center">VISI</h2>
     </div>
     <div>
-        <p class="text-justify" style="margin-left: -200px; margin-right: -200px; padding-bottom: 0px;">
+        <p class="text-justify" >
             LSP P1 BPMJP mempunyai visi menjadi LSP bidang metrologi, mutu, dan jasa perdagangan serta penunjangnya untuk menciptakan sumber daya manusia yang kompeten dan profesional serta diakui secara Nasional, Regional, dan Internasional.
         </p>
     </div>
@@ -123,7 +142,7 @@ p, table, div {
         <h2 class="text-bold text-center">MISI</h2>
     </div>
     <div>
-        <ol style="margin-left: -200px; margin-right: -200px; padding-bottom: 30px;">
+        <ol style="padding-bottom: 30px;">
             <li class="text-justify">Menyelenggarakan sertifikasi kompetensi SDM bidang metrologi, mutu, dan jasa perdagangan serta penunjangnya/pemasok yang independen dan profesional.</li>
             <li class="text-justify">Memastikan dan memelihara kompetensi SDM bidang metrologi, mutu, dan jasa perdagangan untuk dapat bersaing secara nasional regional dan Internasional.</li>
             <li class="text-justify">Menjamin mutu dengan menjaga proses sertifikasi sesuai dengan standar yang berlaku.</li>
@@ -131,8 +150,19 @@ p, table, div {
         </ol>
     </div>
 </div>
-
 </div>
+
+<style>
+@media (max-width: 768px) {
+    p, ol {
+        margin-left: 40px;
+        margin-right: 40px;
+        padding-left: 0;
+        padding-right: 0;
+    }
+}
+</style>
+
 <!--<section class="barcount-section" style="min-height:50px !important">
 	<div class="container">
 		<div class="row row-bar-count py-1 text-white">
@@ -180,14 +210,14 @@ p, table, div {
 	</div>
 </div>-->
 <div class="container-fluid text-white mb-4">
-    <div style="background-color: #3A8F53; margin-left: 185px; margin-right: 185px; border-radius: 10px; padding: 3px;">
+    <div class="responsive-container" style="background-color: #3A8F53; border-radius: 10px; padding: 3px;">
         <div class="container py-3" >
             <h3 style="padding-top:5px;padding-bottom:0px;margin-left:24px;">Skema Sertifikasi Pengujian Meter Air</h3>
         </div>
     </div>
 </div>
 <div class="container">
-    <div class="row my-4" style="margin-left: 110px; margin-right: 110px;">
+    <div class="row my-4 responsive-content">
         <div class="col-md-12">
 			<p class="text-justify">
 			Skema sertifikasi Pengujian Meter Air adalah skema sertifikasi klaster yang dikembangkan oleh Komite Skema LSP P1 BPMJP untuk memenuhi kebutuhan sertifikasi kompetensi kerja di LSP P1 BPMJP.
@@ -234,7 +264,7 @@ p, table, div {
             </tbody>
             </table>
             <br>
-	        <p> Persyaratan Dasar Pemohon Sertifikasi
+	        <p> Persyaratan Dasar Pemohon Sertifikasi:
 		    <ol>
                 <li>Memiliki sertifikat pelatihan pengujian meter air yang diselenggarakan oleh BPMJP.</li>
                 <li>Pendidikan minimal SMA/SMK atau sederajat.</li>
@@ -243,11 +273,6 @@ p, table, div {
             <div class="card p-3 text-center">
             <p>Sesuai dengan PMK Nomor 140 Tahun 2023 Biaya sertifikasi untuk skema Kompetensi Pengujian Meter Air sebesar Rp. 1.250.000,- per orang per hari</p>
             </div>
-            <!--<span class="alert alert-warning text-center">Informasi lebih lanjut dapat menghubungi: 0811-2006-6664</span>-->
-            <!--<p>Informasi lebih lanjut dapat menghubungi: 0811-2006-6664</p>-->
-            <!--<div class="custom-card">
-                <p>Informasi lebih lanjut dapat menghubungi: 0811-2006-6664</p>
-            </div>-->
             <div class="d-flex justify-content-center">
             <div class="alert alert-warning text-center" style="display: inline-block;">
             Informasi lebih lanjut hubungi: <strong>0811-2006-6664</strong>
@@ -256,6 +281,32 @@ p, table, div {
         </div>
     </div>
 </div>
+
+<style>
+/* Untuk layar lebih besar dari 768px */
+.responsive-container {
+    margin-left: 185px;
+    margin-right: 185px;
+}
+
+.responsive-content {
+    margin-left: 110px;
+    margin-right: 110px;
+}
+
+/* Untuk layar lebih kecil dari 768px */
+@media (max-width: 768px) {
+    .responsive-container {
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+
+    .responsive-content {
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+}
+</style>
 
 <div class="mb-1">&nbsp;</div>
 
