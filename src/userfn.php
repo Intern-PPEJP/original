d7a860a348c970eb5d39d646d8c130258dceab36
riekesetiawati <riekesetiawati@apps.ipb.ac.id>
2024-08-30 10:16:27 +0700

    belum

--- src/userfn.php
+++ src/userfn.php
@@ -217,254 +217,195 @@ function myheader(){
 
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 <!-- format phone -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 <link href="<?= GetUrl('dirku/cssku/bootstrap.min.css') ?>" rel="stylesheet">
 <link href="<?= GetUrl('dirku/cssku/bootstrap-icons.css') ?>" rel="stylesheet">
 <link href="<?= GetUrl('dirku/cssku/cscs.css') ?>" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 <style>
-	body,.h1,.h2,.h3,.h4,.h5,.h6,body,h1,h2,h3,h4,h5,h6,p { 
-		font-family: 'Poppins', sans-serif !important; 
-	}
-
-	.main-header #ew-navbar .nav-item { 
-		padding: 10px;
-	}
-
-	.content-header { 
-		background: #031A31; margin-bottom: 15px; 
-	} 
-	
-	.content-header h1 { 
-		color: #ffffff !important 
-	}
-
-	button.close span { 
-		font-size: 25px; 
-		background: #ff1800; 
-		border-radius: 10px; 
-		padding-right: 10px; 
-		padding-left: 10px; 
-	}
-
-	#fcari {
-		position: absolute;
-		z-index: 99;
-		width: 200px !important;
-		left: 900px;
-		top: 10px;
-		flex-grow: 1; /* Agar kotak pencarian mengambil ruang yang tersisa */
-		margin-left: 20px; /* Jarak antara logo dan kotak pencarian */
-		font-size: 16px;
-	}
-
-	
-	.navbar-nav {
-		margin-right: auto; /* Memberikan ruang otomatis antara item menu dan tombol "Daftar Pelatihan" */
-		flex-grow: 1; /* Membiarkan item navigasi tumbuh dan mengambil ruang yang tersedia */
-		display: flex;
-		justify-content: center; /* Memusatkan item navigasi */
-		margin-left: auto;
-	}
-
-	.navbar-collapse {
-		display: flex;
-		justify-content: space-between; /* Menyebar item navbar secara merata di dalam navbar */
-		align-items: center;
-		width: 100%;
-	}
-
-	.navbar {
-		position: fixed;
-		top: 0;
-		width: 100%;
-		z-index: 1000; /* Pastikan navbar berada di atas elemen lain */
-	}
-
-	@media screen and (max-width: 768px) { /* mobile view */
-		#fcari {
-			position: static;
-			width:100% !important;
-			margin-left: 0; /* Reset margin pada tampilan mobile */
-		}
-		
-		.header {
-			flex-direction: column;
-			align-items: flex-start; /* Agar logo dan kotak pencarian berada di atas-bawah */
+    body, h1, h2, h3, h4, h5, h6, p { 
+        font-family: 'Poppins', sans-serif !important; 
     }
-	}
 	
-	.header {
-		display: flex;
-		align-items: center;
-		justify-content: space-between; /* Agar logo dan kotak pencarian berada di ujung yang berlawanan */
-		padding: 10px;
-	}
+.form-inline input {
+    width: 200px;
+    position:relative;
+
+}
+
+    @media screen and (max-width: 768px) {
+        #fcari {
+            position: static;
+            width: 100% !important;
+            margin-left: 0;
+        }
+        
+        .header {
+            flex-direction: column;
+            align-items: flex-start;
+        }
+    }
+    
+    .header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 10px;
+    }
 
     p, table, div, ul, li {
-    	font-size: 16px;
-	}
-	
-	h2{
-		font-size: 20px;
-	}
+        font-size: 16px;
+    }
+    
+    h2 {
+        font-size: 20px;
+    }
 
-	h3{
-		font-size: 18px;
-	}
+    h3 {
+        font-size: 18px;
+    }
 
-	.logo {
-    	margin-left: 50px; /* Ganti angka ini dengan jumlah jarak yang diinginkan */
-		height: auto;
-	}
+    .logo {
+        margin-left: 50px;
+        height: auto;
+    }
 
-	.custom-btn {
-		background-color: #3A8F53; /* Warna latar belakang hijau */
-		color: #fff; /* Warna teks putih */
-		padding: 10px 20px; /* Ruang dalam (padding) di sekitar teks tombol */
-		border-radius: 5px; /* Sudut membulat */
-		border: none; /* Menghilangkan garis tepi */
-		font-size: 16px; /* Ukuran font */
-		font-weight: bold; /* Ketebalan font */
-		transition: background-color 0.3s ease; /* Animasi transisi pada perubahan warna latar belakang */
-		margin-right: 50px;
-	}
+    .custom-btn {
+        background-color: #3A8F53;
+        color: #fff;
+        padding: 10px 20px;
+        border-radius: 5px;
+        border: none;
+        font-size: 16px;
+        font-weight: bold;
+        transition: background-color 0.3s ease;
+        margin-right: 50px;
+    }
 
-	/* Gaya tombol saat di-hover */
-	.custom-btn:hover {
-		background-color: #5BC1AC; /* Warna latar belakang saat tombol di-hover */
-		color: #fff; /* Warna teks tetap putih */
-		text-decoration: none; /* Menghilangkan garis bawah teks */
-		cursor: pointer; /* Ubah kursor menjadi pointer saat di-hover */
-	}
+    .custom-btn:hover {
+        background-color: #5BC1AC;
+        color: #fff;
+        text-decoration: none;
+        cursor: pointer;
+    }
 
-	.icon {
-		height: 15px;
-		width: 15px;
-		margin-right: 10px; /* Menambahkan jarak antara ikon dan teks */
-		vertical-align: middle; /* Agar ikon sejajar dengan teks secara vertikal */
-		display: inline-block; /* Memastikan gambar dan teks berada di satu baris */
-		margin-top: 3px;
-	}
+    .icon {
+        height: 15px;
+        width: 15px;
+        margin-right: 10px;
+        vertical-align: middle;
+        display: inline-block;
+        margin-top: 3px;
+    }
 
-	.footer-item {
-		display: flex; /* Menggunakan Flexbox untuk merapikan tata letak */
-		align-items: flex-start; /* Menyelaraskan gambar dan teks di tengah secara vertikal */
-	}
+    .footer-item {
+        display: flex;
+        align-items: flex-start;
+    }
 
-	.footer-item img {
-		margin-right: 10px; /* Menambahkan jarak antara ikon dan teks */
-	}
+    .footer-item img {
+        margin-right: 10px;
+    }
 
-	.footer-item div {
-		display: inline-block; /* Memastikan teks tetap berada di samping gambar */
-		vertical-align: middle; /* Menyelaraskan teks di tengah secara vertikal */
-	}
-	.social-media-icon {
-		height: 30px; /* Atur ukuran ikon sesuai kebutuhan, misalnya 25px */
-		width: auto; /* Menyesuaikan lebar secara otomatis */
-		margin-right: 5px; /* Memberikan jarak antar ikon */
-	}
+    .footer-item div {
+        display: inline-block;
+        vertical-align: middle;
+    }
+
+    .social-media-icon {
+        height: 30px;
+        width: auto;
+        margin-right: 5px;
+    }
 </style>
 
 <script>
-// Prevent closing from click inside dropdown
-$(document).on('click', '.dropdown-menu', function (e) {
-  e.stopPropagation();
+$(document).ready(function(){
+    $('#cari').attr('disabled',true);
+    $('#psearch').keyup(function(){
+        $('#cari').attr('disabled', $(this).val().length == 0);
+    });
 });
-
-// make it as accordion for smaller screens
-if ($(window).width() < 992) {
-  $('.dropdown-menu a').click(function(e){
-    e.preventDefault();
-      if($(this).next('.submenu').length){
-        $(this).next('.submenu').toggle();
-      }
-      $('.dropdown').on('hide.bs.dropdown', function () {
-     $(this).find('.submenu').hide();
-  })
-  });
-}
 </script>
-<nav class="navbar navbar-expand-lg navbar-light bg-light" style="padding:0;">
-	<a class="navbar-brand" href="<?= GetUrl('home')?>">
-		<img src="<?= GetUrl('images/logo-kemendag.png') ?>" class="logo" alt="PPEJP | Pusat Pelatihan Sumber Daya Manusia Ekspor dan Jasa Perdagangan (PPEJP) merupakan lembaga yang berada di lingkungan Sekretariat Jenderal, Kementerian Perdagangan. PPEJP mempunyai tugas melaksanakan pengembangan sumber daya manusia ekspor, mutu, personil metrologi legal, dan jasa perdagangan untuk dunia usaha dan masyarakat.">
-	</a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-  	</button>
-	<div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-		<ul class="navbar-nav ms-auto mr-3">
-			<li class="nav-item">
-				<a class="nav-link click-scroll" href="<?= GetUrl('home') ?>">Beranda</a>
-			</li>
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tentang Kami</a>
-				<ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
-					<li><a class="dropdown-item" href="<?= GetUrl('tentang-kami') ?>">Pusat Pelatihan SDM Ekspor dan <br>Jasa Perdagangan (PPEJP)</a></li>
-					<li><a class="dropdown-item" href="<?= GetUrl('tentang-bpmjp') ?>">Balai Pelatihan SDM Metrologi, Mutu <br>dan Jasa Perdagangan (BPMJP)</a></li>
-					<li><a class="dropdown-item dropdown-toggle" href="#">Lembaga Sertifikasi Profesi (LSP)</a>
-						<ul class="submenu dropdown-menu">
-							<li><a class="dropdown-item" href="<?= GetUrl('lsp-ppejp') ?>">LSP PPEJP</a></li>
-							<li><a class="dropdown-item" href="<?= GetUrl('lsp-bpmjp') ?>">LSP BPMJP</a></li>
-						</ul>
-					</li>
-				</ul>
-			</li>
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Kegiatan</a>
-				<ul class="dropdown-menu" aria-labelledby="navbarLightDropdownMenuLink" id="sub-pelatihan">
-					<li><a class="dropdown-item dropdown-toggle" href="<?= GetUrl('pelatihan') ?>">Pelatihan</a>
-						<ul class="submenu dropdown-menu">
-							<li><a class="dropdown-item" href="<?= GetUrl('pelatihan-ekspor') ?>">Pelatihan Ekspor</a></li>
-							<li><a class="dropdown-item" href="<?= GetUrl('pelatihan-metrologi') ?>">Pelatihan Metrologi</a></li>
-							<li><a class="dropdown-item" href="<?= GetUrl('pelatihan-mutu') ?>">Pelatihan Mutu</a></li>
-							<li><a class="dropdown-item" href="<?= GetUrl('pelatihan-jasa-perdagangan') ?>">Pelatihan Jasa Perdagangan</a></li>
-						</ul>
-					</li>
-					<li><a class="dropdown-item font-italic" href="<?= GetUrl('export-coaching-program') ?>">Export Coaching Program</a></li>
-					<li><a class="dropdown-item" href="<?= GetUrl('webinar') ?>">Webinar</a></li>
-					<li><a class="dropdown-item" href="https://kudagang.kemendag.go.id/" target="_blank">KUDAGANG</a></li>
-					<li><a class="dropdown-item" href="<?= GetUrl('obrolan-ekspor') ?>">Obrolan Ekspor</a></li>
-					<!--<li><a class="dropdown-item" href="<?= GetUrl('sertifikasikompetensi') ?>">Sertifikasi Kompetensi</a></li> -->
-				</ul>
-			</li>
-			<li class="nav-item dropdown">
-				<a class="nav-link" href="<?= GetUrl('kontak') ?>">Kontak</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="<?= GetUrl('berita') ?>">Berita</a>
-			</li>
-			<?php if (IsLoggedIn()) { 
-				?>
-				<!--<li class="nav-item dropdown">
-					<a class="nav-link" href="akun" style="font-size:35px;padding-top:5px;"><i class="fa fa-user-circle-o" aria-hidden="true"></i></a>
-					<ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
-						<li><a class="dropdown-item" href="logout"><i class="fa fa-logout" aria-hidden="true"></i> Logout</a></li>
-					</ul>
-				</li>-->
-				<?php
-				} else { ?>
-				<!--<li class="nav-item">
-					<a class="nav-link" href="login" style=""><i class="fa fa-user" aria-hidden="true"></i> Masuk</a>
-				</li>-->
-				<?php } ?>
-		</ul>
-		<a class="nav-link custom-btn btn" href="<?= GetUrl('formpendaftaran') ?>"><p>Daftar Pelatihan</p></a>	
-	</div>
-	<form name="fcari" id="fcari" class="ew-form ew-login-form" action="<?= GetUrl('caridatalist') ?>" method="get">
-		<div class="input-group p-2">
-			<input type="hidden" name="cmd" value="search">
-			<input type="hidden" name="t" value="caridata">
-			<input type="text" class="form-control" id="psearch" name="psearch" placeholder="Pencarian..." aria-label="Pencarian..." aria-describedby="basic-addon2" value="<?php echo @$_GET["psearch"]; ?>" style="min-width:80px">
-			<div class="input-group-append">
-				<button class="btn btn-default" id="cari" type="submit" value="cari" style=" border: 1px solid #bbb; "><i class="fas fa-search" aria-hidden="true"></i> </button>
-			</div>
-		</div>
-	</form>
-</nav>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script>
+        $('.dropdown-submenu a.dropdown-toggle').on("click", function(e) {
+            var $el = $(this).next('.dropdown-menu');
+            var $parent = $(this).closest('.dropdown-menu');
+            $parent.find('.dropdown-menu').not($el).hide();
+            $el.toggle();
+            e.stopPropagation();
+            e.preventDefault();
+        });
+	</script>
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" id="mainNav">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="<?= GetUrl('home')?>">
+                <img src="<?= GetUrl('images/logo-kemendag.png') ?>" alt="logo">
+            </a>
+            <form name="fcari" id="fcari" class="form-inline my-2 my-lg-0" action="<?= GetUrl('caridatalist') ?>" method="get">
+                <input class="form-control mr-sm-2" type="search" id="psearch" name="psearch" placeholder="Pencarian..." aria-label="Search" value="<?php echo @$_GET['psearch']; ?>" style="min-width: 80px;">
+                <button class="btn btn-outline-success my-2 my-sm-0"  id="cari" type="submit" value="cari">Cari</button>
+            </form>
 
-<br>
-<br>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
+                            Tentang Kami
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="<?= GetUrl('tentang-kami') ?>">Pusat Pelatihan SDM Ekspor dan Jasa Perdagangan (PPEJP)</a></li>
+                            <li><a class="dropdown-item" href="<?= GetUrl('tentang-bpmjp') ?>">Balai Pelatihan SDM Metrologi, Mutu dan Jasa Perdagangan (BPMJP)</a></li>
+                            <li class="dropdown-submenu">
+                                <a class="dropdown-item dropdown-toggle" href="#">Lembaga Sertifikasi Profesi (LSP)</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="<?= GetUrl('lsp-ppejp') ?>">LSP PPEJP</a></li>
+                                    <li><a class="dropdown-item" href="<?= GetUrl('lsp-bpmjp') ?>">LSP BPMJP</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-expanded="false">
+                            Kegiatan
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown2">
+                            <li class="dropdown-submenu">
+                                <a class="dropdown-item dropdown-toggle" href="<?= GetUrl('pelatihan') ?>">Pelatihan</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="<?= GetUrl('pelatihan-ekspor') ?>">Pelatihan Ekspor</a></li>
+                                    <li><a class="dropdown-item" href="<?= GetUrl('pelatihan-metrologi') ?>">Pelatihan Metrologi</a></li>
+                                    <li><a class="dropdown-item" href="<?= GetUrl('pelatihan-mutu') ?>">Pelatihan Mutu</a></li>
+                                    <li><a class="dropdown-item" href="<?= GetUrl('pelatihan-jasa-perdagangan') ?>">Pelatihan Jasa</a></li>
+                                </ul>
+                            </li>
+                            <li><a class="dropdown-item" href="<?= GetUrl('export-coaching-program') ?>">Export Coaching Program</a></li>
+                            <li><a class="dropdown-item" href="<?= GetUrl('webinar') ?>">Webinar</a></li>
+                            <li><a class="dropdown-item" href="https://kudagang.kemendag.go.id/" target="_blank" >KUDAGANG</a></li>
+                            <li><a class="dropdown-item" href="<?= GetUrl('obrolan-ekspor') ?>">Obrolan Ekspor</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= GetUrl('kontak') ?>">Kontak</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= GetUrl('berita') ?>">Berita</a>
+                    </li>
+                </ul>
+                <a class="btn btn-success ml-lg-3 mt-2 mt-lg-0" href="Signin.php" role="button">Masuk</a>
+            </div>
+        </div>
+    </nav>
 
 <script>
 $(document).ready(function(){
@@ -477,6 +418,7 @@ $(document).ready(function(){
     })
 });
 </script>
+</div>
 <?php
 }
 
