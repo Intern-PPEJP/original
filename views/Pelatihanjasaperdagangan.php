a89edf9edfa64adb6de4b6180b427b2a7746b25c
ansalma <anandaslm04@gmail.com>
2024-09-10 13:06:29 +0700

    update tinggi bg img 450px

--- views/Pelatihanjasaperdagangan.php
+++ views/Pelatihanjasaperdagangan.php
@@ -19,7 +19,7 @@ $Pelatihanjasaperdagangan = &$Page;
 
 <div class="container-fluid" style="xmargin-top:90px;">
     <div class="row">
-        <div class="col-md-12 p-0" style=" height: 400px">
+        <div class="col-md-12 p-0" style=" height: 450px">
             <div style="background-image: url(images/pages/pelatihan-jasa-perdagangan.png); background-size: cover ; background-position: center;width: 100%;height: 100%; position: absolute;top:0">
             </div>
         </div>
